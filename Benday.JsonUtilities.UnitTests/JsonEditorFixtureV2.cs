ba35fada177511514d33f177478de00fada826c2
Benjamin Day <benday@benday.com>
2023-03-17 10:00:37 -0400

    Created jsoneditorv2 to start refactoring

---
+++ Benday.JsonUtilities.UnitTests/JsonEditorFixtureV2.cs
@@ -0,0 +1,310 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Benday.JsonUtilities.UnitTests;
+
+[TestClass]
+public class JsonEditorV2Fixture : UnitTestBase
+{
+    [TestInitialize]
+    public void OnTestInitialize()
+    {
+        _SystemUnderTest = null;
+        _PathToSampleConfigFile = null;
+    }
+
+    string _PathToSampleConfigFile;
+
+    private void InitializeWithEmptyJson()
+    {
+        _PathToSampleConfigFile = CreateSampleJsonConfigFileEmpty();
+
+        _SystemUnderTest = new JsonEditorV2(_PathToSampleConfigFile);
+    }
+
+    private void InitializeWithPopulatedJson()
+    {
+        _PathToSampleConfigFile = CreateSampleJsonConfigFile();
+
+        Console.WriteLine(_PathToSampleConfigFile);
+
+        _SystemUnderTest = new JsonEditorV2(_PathToSampleConfigFile);
+    }   
+
+    private JsonEditorV2 _SystemUnderTest;
+    public JsonEditorV2 SystemUnderTest
+    {
+        get
+        {
+            if (_SystemUnderTest == null)
+            {
+                throw new InvalidOperationException(
+                    "System under test has not been initialized yet.");
+            }
+
+            return _SystemUnderTest;
+        }
+    }
+
+    [TestMethod]
+    public void GetValue_OneLevel()
+    {
+        InitializeWithPopulatedJson();
+
+        var expected = "FirstLevelValue";
+
+        var actual = SystemUnderTest.GetValue(
+            "FirstLevel");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+    }
+
+    [TestMethod]
+    public void GetValue_TwoLevels()
+    {
+        InitializeWithPopulatedJson();
+
+        var expected = "default-connection-string-value";
+
+        var actual = SystemUnderTest.GetValue(
+            "ConnectionStrings", "DefaultConnectionString");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+    }
+
+    [TestMethod]
+    public void GetValue_ThreeLevels()
+    {
+        InitializeWithPopulatedJson();
+
+        var expected = "Debug";
+
+        var actual = SystemUnderTest.GetValue(
+            "Logging", "LogLevel", "Default");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+    }
+
+
+    [TestMethod]
+    public void Json_NewValue_SetValue_OneLevel()
+    {
+        InitializeWithEmptyJson();
+
+        var expected = "new value";
+
+        SystemUnderTest.SetValue(
+            expected, "FirstLevel");
+
+        var actual = SystemUnderTest.GetValue(
+            "FirstLevel");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+    }
+
+    [TestMethod]
+    public void Json_NewValue_SetValue_TwoLevels()
+    {
+        InitializeWithEmptyJson();
+
+        var expected = "new value";
+
+        SystemUnderTest.SetValue(
+            expected, "FirstLevel", "Sub1");
+
+        SystemUnderTest.SetValue(
+            expected, "FirstLevel", "Sub2");
+
+        var actual = SystemUnderTest.GetValue(
+            "FirstLevel", "Sub1");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+
+        var actual2 = SystemUnderTest.GetValue(
+            "FirstLevel", "Sub2");
+
+        Assert.AreEqual<string>(expected, actual2, "Wrong value.");
+    }
+
+    [TestMethod]
+    public void Json_ExistingValue_SetValue_OneLevel()
+    {
+        InitializeWithPopulatedJson();
+
+        var expected = "new-FirstLevelValue";
+
+        SystemUnderTest.SetValue(
+            expected, "FirstLevel");
+
+        var actual = SystemUnderTest.GetValue(
+            "FirstLevel");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+    }
+
+    [TestMethod]
+    public void Json_ExistingValues_SetValue_DoesNotClobberUnrelatedValues()
+    {
+        InitializeWithPopulatedJson();
+
+        var original =
+            SystemUnderTest.GetValue(
+                "ConnectionStrings", "DefaultConnectionString");
+
+        UnitTestUtility.AssertIsNotNullOrEmptyString(original, "Original value");
+
+
+        SystemUnderTest.SetValue(
+                "val1", "ConnectionStrings", "conn1");
+
+        SystemUnderTest.SetValue(
+                "val2", "ConnectionStrings", "conn2");
+
+        var actual1 = SystemUnderTest.GetValue("ConnectionStrings", "conn1");
+        var actual2 = SystemUnderTest.GetValue("ConnectionStrings", "conn2");
+
+        Assert.AreEqual<string>("val1", actual1, "Wrong value.");
+        Assert.AreEqual<string>("val2", actual2, "Wrong value.");
+
+
+        var reloaded =
+            SystemUnderTest.GetValue(
+                "ConnectionStrings", "DefaultConnectionString");
+
+        Assert.AreEqual<string>(original, reloaded, "Original value got messed with.");
+    }
+
+
+    [TestMethod]
+    public void Json_NewValues_SetValue_ConnectionStrings()
+    {
+        InitializeWithEmptyJson();
+
+        SystemUnderTest.SetValue(
+                "val1", "ConnectionStrings", "conn1");
+
+        SystemUnderTest.SetValue(
+                "val2", "ConnectionStrings", "conn2");
+
+        var actual1 = SystemUnderTest.GetValue("ConnectionStrings", "conn1");
+        var actual2 = SystemUnderTest.GetValue("ConnectionStrings", "conn2");
+
+        Assert.AreEqual<string>("val1", actual1, "Wrong value.");
+        Assert.AreEqual<string>("val2", actual2, "Wrong value.");
+    }
+
+
+    [TestMethod]
+    public void Json_ExistingValue_SetValue_TwoLevels()
+    {
+        InitializeWithPopulatedJson();
+
+        var expected = "new-default-connection-string-value";
+
+        SystemUnderTest.SetValue(
+            expected, "ConnectionStrings", "DefaultConnectionString");
+
+        var actual = SystemUnderTest.GetValue(
+            "ConnectionStrings", "DefaultConnectionString");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+    }
+
+    [TestMethod]
+    public void Json_ExistingValue_SetValue_ThreeLevels()
+    {
+        InitializeWithPopulatedJson();
+
+        var expected = "new-Debug";
+
+        SystemUnderTest.SetValue(
+            expected, "Logging", "LogLevel", "Default");
+
+        var actual = SystemUnderTest.GetValue(
+            "Logging", "LogLevel", "Default");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+    }
+
+    [TestMethod]
+    public void Json_ExistingValue_SetValue_ThreeLevels_v2()
+    {
+        InitializeWithPopulatedJson();
+
+        var expected = "new-Debug";
+
+        SystemUnderTest.SetValue(
+            expected,
+            "Logging", "LogLevel", "Default");
+
+        var actual = SystemUnderTest.GetValue(
+            "Logging", "LogLevel", "Default");
+
+        Assert.AreEqual<string>(expected, actual, "Wrong value.");
+    }
+
+    [TestMethod]
+    public void GetSiblingValue()
+    {
+        // arrange
+        _PathToSampleConfigFile = CreateSampleAuthMeFile();
+   
+        Console.WriteLine(_PathToSampleConfigFile);
+
+        var contents = File.ReadAllText(_PathToSampleConfigFile);
+
+        var jsonArray = JArray.Parse(contents);
+
+        _SystemUnderTest = new JsonEditorV2(jsonArray[0].ToString(), true);
+
+        var expected = "iat-value";
+
+        var args = new SiblingValueArguments
+        {
+            SiblingSearchKey = "typ",
+            SiblingSearchValue = "iat",
+
+            DesiredNodeKey = "val",
+            PathArguments = new[] { "user_claims" }
+        };
+
+        // act
+        var actual = SystemUnderTest.GetSiblingValue(args);
+
+        // assert
+        Assert.AreEqual<string>(expected, actual, "Value was wrong");
+    }
+
+    [TestMethod]
+    public void SetSiblingValue_UpdateExisting()
+    {
+        // arrange
+        _PathToSampleConfigFile = CreateSampleAuthMeFile();
+
+        Console.WriteLine(_PathToSampleConfigFile);
+
+        var contents = File.ReadAllText(_PathToSampleConfigFile);
+
+        var jsonArray = JArray.Parse(contents);
+
+        _SystemUnderTest = new JsonEditorV2(jsonArray[0].ToString(), true);
+
+        var expected = "iat-value-new";
+
+        var args = new SiblingValueArguments
+        {
+            SiblingSearchKey = "typ",
+            SiblingSearchValue = "iat",
+
+            DesiredNodeKey = "val",
+            PathArguments = new[] { "user_claims" },
+            DesiredNodeValue = expected
+        };
+
+        // act
+        SystemUnderTest.SetSiblingValue(args);
+
+        // assert
+        var actual = SystemUnderTest.GetSiblingValue(args);
+        Assert.AreEqual<string>(expected, actual, "Value was wrong");
+    }    
+}
